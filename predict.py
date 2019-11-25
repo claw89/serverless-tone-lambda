e7eb079b867953b67b4f8e94791a5be4e4db8546
claw89 <christopher.law89@gmail.com>
2019-11-25 04:49:45 +0000

    First commit

---
+++ predict.py
@@ -0,0 +1,130 @@
+import os
+import json
+import pickle
+
+try:
+  import unzip_requirements
+except ImportError:
+  pass
+
+import numpy as np
+from scipy import signal
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, LSTM, Dense, Dropout
+
+def get_param_from_url(event, param_name):
+    """
+    Helper function to retrieve query parameters from a Lambda call. Parameters are passed through the
+    event object as a dictionary.
+
+    :param event: the event as input in the Lambda function
+    :param param_name: the name of the parameter in the query string
+    :return: the parameter value
+    """
+    params = event['queryStringParameters']
+    return params[param_name]
+
+def get_data_from_url(event):
+    data = event['body']
+    data = json.loads(data)
+    return np.array(data)
+
+def load_model():
+    encoder_inputs = Input(shape=(None, 86))
+    encoder = LSTM(256, activation='relu', return_state=True)
+    encoder_outputs, _, _ = encoder(encoder_inputs)
+    dropout_layer = Dropout(0.5)
+    dropout_output = dropout_layer(encoder_outputs)
+    output_layer = Dense(5, activation='softmax')
+    outputs = output_layer(dropout_output)
+
+    model = Model(encoder_inputs, outputs)
+    model.load_weights('/var/task/models/seq_weights')
+
+    return model
+
+def trim_silences(Sxx):
+    """
+    Removes leading and trailing silence from a spectrogram
+    Silence is defined as a maximum amplitude less than 1% of 
+    the overall spectrogram maximum amplitude
+    Returns the trimmed spectrogram
+    """
+    non_silent = list(np.max(Sxx, axis=0) > np.max(Sxx) * 0.01)
+    start_id = non_silent.index(True)
+    stop_id = len(non_silent) - 1 - non_silent[::-1].index(True)
+
+    return Sxx[:, start_id:stop_id]
+
+def split_spectrogram(Sxx):
+    """
+    Divides the spectrogram of a two sylable word into its component spectrograms
+    Returns a list [Spectrogram for sylable 1, Spectrogram for sylable 2]
+    """
+    q1 = Sxx.shape[1]//4
+    q3 = 3*q1
+    center = np.argmin(Sxx[:, q1:q3].max(axis=0))
+
+    return trim_silences(Sxx[:, :q1+center]), trim_silences(Sxx[:, q1+center:])
+
+def spectrogram(data, num_syl):
+    """
+    data: a numpy array containing raw audio data
+    """
+
+    spectrograms = []
+    shapes = []
+
+    if len(data.shape) > 1:
+        data = data.T[0]
+
+    fs = 44000
+    f, t, Sxx = signal.spectrogram(data, fs, nperseg=1024, nfft=1024*4)
+    Sxx = Sxx[:86]
+
+    Sxx = trim_silences(Sxx)
+
+    if num_syl == 1:
+        Sxx = Sxx / np.max(Sxx)
+        Sxx = np.expand_dims(Sxx.T, 0)
+        spectrograms.append(Sxx)
+        shapes.append(json.dumps(Sxx.shape))
+
+    elif num_syl == 2:
+        Sxx1, Sxx2 = split_spectrogram(Sxx)
+
+        Sxx1 = Sxx1 / np.max(Sxx1)
+        Sxx1 = np.expand_dims(Sxx1.T, 0)
+        spectrograms.append(Sxx1)
+        shapes.append(json.dumps(Sxx1.shape))
+
+        Sxx2 = Sxx2 / np.max(Sxx2)
+        Sxx2 = np.expand_dims(Sxx2.T, 0)
+        spectrograms.append(Sxx2)
+        shapes.append(json.dumps(Sxx2.shape))
+
+    return spectrograms, shapes
+
+def handler(event, context):
+    model = load_model()
+
+    num_syl = json.loads(get_param_from_url(event, 'num_syl'))
+    data = get_data_from_url(event)
+
+    spectrograms, shapes = spectrogram(data, num_syl)
+
+    predictions = []
+    for Sxx in spectrograms:
+        predictions.append(model.predict(Sxx)[0].tolist())
+
+    body = {"predictions": json.dumps(predictions),
+            "spectrograms": json.dumps([Sxx.tolist() for Sxx in spectrograms]),
+            "shapes": json.dumps(shapes)}
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
